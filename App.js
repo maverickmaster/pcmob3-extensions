9ba23a95032248f347ceb77bea1a75bc86e8dc31
milton <milton@miltons-MacBook.local>
2020-12-11 11:40:30 +0800

    move add Screen to seperate screen

--- App.js
+++ App.js
@@ -9,116 +9,14 @@ import {
 } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
 import * as SQLite from "expo-sqlite";
+import AddScreen from "./screens/AddScreen";
+import NotesScreen from "./screens/NotesScreen";
+import NotesStack from "./screens/NotesStack";
 
 const db = SQLite.openDatabase("notes.db");
 
-function NotesScreen({ navigation }) {
-  const [notes, setNotes] = useState([
-    { title: "Walk the cat", done: false, id: "0" },
-    { title: "Feed the elephant", done: false, id: "1" },
-  ]);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote}>
-          <Ionicons
-            name="ios-create-outline"
-            size={30}
-            color="black"
-            style={{
-              color: "#f55",
-              marginRight: 10,
-            }}
-          />
-        </TouchableOpacity>
-      ),
-    });
-  });
-
-  function addNote() {
-    navigation.navigate("Add Screen");
-    //const newNote = {
-    //title: "Sample new note",
-    //done: false,
-    //id: notes.length.toString(),
-    //};
-    //setNotes([...notes, newNote]);
-  }
-
-  function renderItem({ item }) {
-    return (
-      <View
-        style={{
-          padding: 10,
-          paddingTop: 20,
-          paddingBottom: 20,
-          borderBottomColor: "#ccc",
-          borderBottomWidth: 1,
-        }}
-      >
-        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
-      </View>
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        style={{ width: "100%" }}
-        data={notes}
-        renderItem={renderItem}
-      />
-    </View>
-  );
-}
-
 const InnerStack = createStackNavigator();
-
-function NotesStack() {
-  return (
-    <InnerStack.Navigator>
-      <InnerStack.Screen
-        name="Notes"
-        component={NotesScreen}
-        options={{
-          headerTitle: "Notes App",
-          headerTitleStyle: {
-            fontWeight: "bold",
-            fontSize: 30,
-          },
-          headerStyle: {
-            height: 120,
-            backgroundColor: "yellow",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-          },
-        }}
-      />
-    </InnerStack.Navigator>
-  );
-}
-
-function AddScreen({ navigation }) {
-  return (
-    <View style={[styles.container, { backgroundColor: "white" }]}>
-      <Text style={{ fontSize: 24 }}>This is the add screen</Text>
-      <TouchableOpacity
-        style={{
-          padding: 10,
-          backgroundColor: "orange",
-          borderRadius: 5,
-          marginTop: 30,
-        }}
-        onPress={() => navigation.goBack()}
-      >
-        <Text>Dismiss</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
