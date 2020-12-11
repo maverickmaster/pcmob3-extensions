b557abf6be2f28782412e8e0482dec85cbaced7d
milton <milton@miltons-MacBook.local>
2020-12-11 13:34:43 +0800

    added insert to notes screens

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import React from "react";
+
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import * as SQLite from "expo-sqlite";
@@ -26,35 +19,7 @@ export default function App() {
       <Stack.Navigator headerMode="none" mode="modal">
         <Stack.Screen name="Notes Stack" component={NotesStack} />
         <Stack.Screen name="Add Screen" component={AddScreen} />
-        <Stack.Screen
-          name="Notes"
-          component={NotesScreen}
-          options={{
-            title: "Notes, a Todo App",
-            headerStyle: {
-              backgroundColor: "yellow",
-              height: 100,
-              shadowColor: "black",
-              shadowOpacity: 0.2,
-              shadowRadius: 5,
-            },
-            headerTintColor: "#f55",
-            headerTitleStyle: {
-              fontSize: 24,
-              fontWeight: "bold",
-            },
-          }}
-        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
